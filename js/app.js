9e1afba958642d1552ff73f2c6b84c12d9e6744f
Antonnio Ricardez <antonio.ricardez@gmail.com>
2018-11-28 15:37:02 -0600

    ya guarda tokio

--- js/app.js
+++ js/app.js
@@ -1,15 +1,52 @@
 //obtener referencia del elemento
 var btnActualizar = document.getElementById('btnActualizar');
+var btnGuardar = document.getElementById('btnGuardar');
 
 //aregar un listener al boton
 btnActualizar.addEventListener('click',actualizar);
-/*
-function actualizar()
-{
-  alert('actualizar listado');
-}
-*/
+btnGuardar.addEventListener('click', guardar);
+
 actualizar();
+function guardar()
+{
+  var xhttp = new XMLHttpRequest();
+  var data = new FormData();
+  var first_name = document.getElementById('first_name').value;
+  var last_name = document.getElementById('last_name').value;
+  var email = document.getElementById('email').value;
+  var phone_number = document.getElementById('phone_number').value;
+
+  data.append('first_name', first_name );
+  data.append('last_name', last_name );
+  data.append('email', email );
+  data.append('phone_number', phone_number);
+
+  xhttp.onreadystatechange = function()
+  {
+  if (this.readyState == 4 && this.status == 200)
+    {
+
+      var response = JSON.parse(this.responseText);
+      if (response.status == "error")
+      {
+        alert( response.errors[0] );
+      } else{
+        //alert(this.responseText);
+        alert("se guardò");
+        if(response.status == "ok")
+        {
+          document.getElementById('first_name').value = '';
+          document.getElementById('last_name').value = '';
+          document.getElementById('email').value = '';
+          document.getElementById('phone_number').value = '';
+
+        }
+      }
+    }
+  };
+xhttp.open("POST","http://nyc.pixan.io/ajax/public/api/students", true);
+xhttp.send(data);
+}
 function actualizar()
 {
   var xhttp = new XMLHttpRequest();
@@ -24,37 +61,19 @@ function actualizar()
         document.getElementsByTagName('tbody')[0].innerHTML = "";
         response.students.forEach(function(student)
         {
-          /*alert(student.first_name);*/
 
-          var row = document.createElement("tr");
-          var idCell = document.createElement("td");
-          var firstNameCell = document.createElement("td");
-          var lastNameCell = document.createElement("td");
 
-          var idText = document.createTextNode(student.id);
-          var firstNameText = document.createTextNode(student.first_name);
-          var lastNameText = document.createTextNode(student.last_name);
-
-          idCell.appendChild(idText);
-          firstNameCell.appendChild(firstNameText);
-          lastNameCell.appendChild(lastNameText);
-
-          row.appendChild(idCell);
-          row.appendChild(firstNameCell);
-          row.appendChild(lastNameCell);
-
-          document.getElementsByTagName('tbody')[0].appendChild(row);
-
-          /*
-          var html= "<tr><td>"
+          var html = "<tr><td>"
             + student.id
             + "</td><td>"
             + student.first_name
             + "</td><td>"
             + student.last_name
+            + "</td><td><button idstudent=\"" + student.last_name
+            + "\" class=\"borrar\">&#9003; Borrar </button>"
             + "</td></tr>";
-            document.getElementsByTagName('tbody')[0].appendChild(html);
-            */
+          document.getElementsByTagName('tbody')[0].innerHTML += html;
+
         });
 
       }
